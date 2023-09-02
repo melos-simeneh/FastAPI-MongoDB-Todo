ba94bec75576eb2466af9ed3636e7c8d032de73e
Melos <85923940+melos-simeneh@users.noreply.github.com>
2023-09-02 10:50:51 -0700

    update todo endpoint

--- routes/route.py
+++ routes/route.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from models.todos import Todo
 from config.db import collection_name
 from schemas.schemas import list_serial
+from bson import ObjectId 
 
 router =APIRouter()
 
@@ -15,3 +16,8 @@ async def get_todos():
 @router.post("/")
 async def post_todo(todo:Todo):
     collection_name.insert_one(dict(todo))
+
+#PUT Request Method
+@router.put("/{id}")
+async def put_tdo(id:str,todo:Todo):
+    collection_name.find_one_and_update({"_id":ObjectId(id)},{"$set":dict(todo)})
